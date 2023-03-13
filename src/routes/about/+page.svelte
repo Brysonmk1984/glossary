3086d4764b2de5739bc4e7cc4625ab353fb28277
Bryson Kruk <brysonmk1984@gmail.com>
2023-03-13 16:20:48 -0600

    added about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,23 +4,36 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1>A Glossary by humans, for humans</h1>
 
 	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
+		For those following along on this wild ride called reality in 2023, it's clear that there's mounting censorship, escalating 'cancel culture', propaganda as a norm by the so called 'news' outlets, false flags by governments, and truth-seekers are misdirected everywhere they look.
 	</p>
 
 	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
+		It's not all hopeless. Various sites out there seem to have a genuine desire to expose the lies and speak truth to the best of their ability. For these brave souls, the reality of the situation goes deep, depressing and sometimes difficult to convey. Simple answers become frustratingly difficult to express to those just learning. Why? These truths diverge from the mainstream narrative in unthinkable ways, interconnections between people and organizations, and the overall magnitude of the problem.
 	</p>
+
+	<p>
+		This is where a <strong>glossary</strong> comes in. There's plenty of amazing journalists out there doing real reporting on the lies, but they don't always have the time or space to explain definitions. New words and phrases are even being coined almost weekly as we discover more information. This glossary is to serve those journalists and anyone who wishes to help share knowledge.
+	</p>
+
+	<p>Does this site have all the answers? No. Is it guaranteed to always be 100% factually correct? No. But it's an honest effort by real people.</p>
+	
+	<p>Some promises to you, the reader:</p>
+	<ul>
+		<li>Not involved in governments, NGOs, or corporations</li>
+		<li>Not here to collect information or sell data</li>
+		<li>Not here to make a profit in any way</li>
+		<li>Politically agnostic (the left-right paradigm is a trap)</li>
+		<li>100% by humans for humans. (No A.I. entries)</li>
+	</ul>
 </div>
+
+
+<style>
+	ul {
+		list-style: initial;
+		margin-left: 30px;
+	}
+</style>
