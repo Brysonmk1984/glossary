406b966388459863fcd886eb6faae696facda848
Bryson Kruk <brysonmk1984@gmail.com>
2023-03-17 07:24:08 -0600

    WIP form

--- src/routes/user-entry/+page.svelte
+++ src/routes/user-entry/+page.svelte
@@ -6,11 +6,34 @@
 <h1>Contribute a definition</h1>
 
 <div>
-  <strong>coming soon</strong>
+  <form method="POST">
+    <div>
+      <input name="author" type="text" placeholder="author" />
+      <input name="entry" type="text" placeholder="entry" />
+      <input name="slug" type="text" placeholder="slug" disabled />
+      <input name="link" type="text" placeholder="link" />
+      <textarea name="description" placeholder="Entry Definition"></textarea>
+      <textarea name="advanced_description" placeholder="Context"></textarea>
+      <button>Send</button>
+    </div>
+  </form>
 </div>
 
 <style>
   div {
     margin:20px auto 0px;
+  }
+
+  form div input{
+    width: 100%;
+    margin: 10px 0px;
+  }
+  form div textarea{
+    width: 100%;
+    margin: 10px 0px;
+  }
+  form div button{
+    display:block;
+    width: 100%
   }
 </style>
