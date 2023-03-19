89257ef4d613f50e3d9f0d9f0a0ee6f9d7cb546e
Bryson Kruk <brysonmk1984@gmail.com>
2023-03-19 13:48:43 -0600

    added links and cleaned up bottom section of definition page

--- src/routes/definitions/[slug]/+page.svelte
+++ src/routes/definitions/[slug]/+page.svelte
@@ -2,6 +2,7 @@
 import DefinitionSection from "$lib/components/DefinitionSection.svelte";
 import ContextSection from "$lib/components/ContextSection.svelte";
 import TagsSection from "$lib/components/TagsSection.svelte";
+import { formatDate } from "$lib/utils/date.util";
 
   export let data : GlossaryDefinition;
 </script>
@@ -13,21 +14,54 @@ import TagsSection from "$lib/components/TagsSection.svelte";
 
   <ContextSection context={data.advanced_description} />
 
+  <div class="bottom-row">
+    {#if !!data.tags?.length}
+      <TagsSection tags={data.tags} />
+    {/if}
+    
+    {#if !!data.link}
+      <div>
+          <h3>Links</h3>
+          <p>
+            <a href={data.link}>Relevant Link</a>
+          </p>
+      </div>
+    {/if}
 
-  <!-- <h3>Author</h3>
-  <dl>
-    <span>
-      <dt>Entry Author:</dt>
-      <dd>{data.author}</dd>
-    </span>
-    <dt>Date Added:</dt>
-    <dd>{data.createdAt}</dd>
-  </dl> -->
-
-  {#if !!data.tags?.length}
-    <TagsSection tags={data.tags} />
-  {/if}
+    <div>
+      <h3>Author</h3>
+      <dl>
+        <span>
+          {data.author}
+        </span>
+        <span class="date">
+          {formatDate(data.createdAt)}
+        </span>
+      </dl>
+    </div>
+ 
+  </div>
+  
 
 {:else}
   <strong>Loading...</strong>
 {/if }
+
+
+<style>
+  .bottom-row{
+    display:flex;
+    width:100%;
+    justify-content: space-between;
+  }
+  dl span {
+    display:block;
+  }
+
+  dl span.date{
+  
+    opacity:.5;
+    font-size: .8em;
+
+  }
+</style>
