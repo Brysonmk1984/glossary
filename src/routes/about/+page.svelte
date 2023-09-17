ae8a0c467045874b8a6fb3d3f73dd5ea946adf4c
Bryson Kruk <brysonmk1984@gmail.com>
2023-09-17 08:19:40 -0600

    update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,36 +4,8 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>A Glossary by humans, for humans</h1>
 
-	<p>
-		For those following along on this wild ride called reality in 2023, it's clear that there's mounting censorship, escalating 'cancel culture', propaganda as a norm by the so called 'news' outlets, false flags by governments, and truth-seekers are misdirected everywhere they look.
-	</p>
-
-	<p>
-		It's not all hopeless. Various sites out there seem to have a genuine desire to expose the lies and speak truth to the best of their ability. For these brave souls, the reality of the situation goes deep, depressing and sometimes difficult to convey. Simple answers become frustratingly difficult to express to those just learning. Why? These truths diverge from the mainstream narrative in unthinkable ways, interconnections between people and organizations, and the overall magnitude of the problem.
-	</p>
-
-	<p>
-		This is where a <strong>glossary</strong> comes in. There's plenty of amazing journalists out there doing real reporting on the lies, but they don't always have the time or space to explain definitions. New words and phrases are even being coined almost weekly as we discover more information. This glossary is to serve those journalists and anyone who wishes to help share knowledge.
-	</p>
-
-	<p>Does this site have all the answers? No. Is it guaranteed to always be 100% factually correct? No. But it's an honest effort by real people.</p>
-	
-	<p>Some promises to you, the reader:</p>
-	<ul>
-		<li>Not involved in governments, NGOs, or corporations</li>
-		<li>Not here to collect information or sell data</li>
-		<li>Not here to make a profit in any way</li>
-		<li>Politically agnostic (the left-right paradigm is a trap)</li>
-		<li>100% by humans for humans. (No A.I. entries)</li>
-	</ul>
 </div>
 
 
-<style>
-	ul {
-		list-style: initial;
-		margin-left: 30px;
-	}
-</style>
+
